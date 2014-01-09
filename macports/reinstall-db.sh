49d7caeee6d509563ac78aa82815c77f8f44d3dc
Nikita Kovaliov <nikita@maizy.ru>
2014-01-09 16:57:33 +0400

    add python postgres packages to reinstall-db.sh

--- macports/reinstall-db.sh
+++ macports/reinstall-db.sh
@@ -6,7 +6,11 @@
 sudo port install \
     postgresql92 \
     postgresql92-server \
-    postgresql_select
+    postgresql_select \
+    py26-psycopg2 \
+    py27-psycopg2 \
+    py32-psycopg2 \
+    py33-psycopg2
     # mysql55 \
     # mysql55-server \
     # mysql_select \
