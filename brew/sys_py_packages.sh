45d3e80446e1a1255e6134c9632e329793c5889a
Nikita Kovaliov <nikita@maizy.ru>
2014-02-14 09:17:45 +0400

    migrate to brew: some libs  & utils, python yolk

--- brew/sys_py_packages.sh
+++ brew/sys_py_packages.sh
@@ -7,36 +7,38 @@ if [ "$UP" == "up" ];then
 fi
 
 function i {
-    pip2.6 install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip2.6 install $1 $2
     #pip3.1 install $1 $2
-    pip3.2 install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip3.2 install $1 $2
 
-    /usr/local/bin/pip3 install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib /usr/local/bin/pip3 install $1 $2
 
     # overwrite all binaries with 2.7 versions
-    /usr/local/bin/pip install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib /usr/local/bin/pip install $1 $2
 }
 
 function i2 {
-    pip2.6 install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip2.6 install $1 $2
 
     # overwrite all binaries with 2.7 versions
-    /usr/local/bin/pip install $1 $2
+    CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib /usr/local/bin/pip install $1 $2
 }
 
 i $upgrade setuptools
 i $upgrade pip
+i $upgrade cython
 i $upgrade bpython
 i $upgrade ipython
 i $upgrade lxml
 i $upgrade pep8
 i $upgrade nose
 i $upgrade virtualenv
+i $upgrade docutils
 
 # virtualenvwrapper not supported in py3.2
-pip2.6 install $1
-/usr/local/bin/pip3 install $1 virtualenvwrapper
-/usr/local/bin/pip install $1 virtualenvwrapper
+CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib pip2.6 install $1
+CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib /usr/local/bin/pip3 install $1 virtualenvwrapper
+CFLAGS=-I$(brew --prefix)/include LDFLAGS=-L$(brew --prefix)/lib /usr/local/bin/pip install $1 virtualenvwrapper
 
 i2 $upgrade fabric
 i2 $upgrade pync
