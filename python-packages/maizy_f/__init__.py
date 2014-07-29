71fc8aa645e9f1ea9231fc263efc3ed9c6dd1f58
Nikita Kovaliov <nikita@maizy.ru>
2014-07-29 13:16:23 +0400

    maizy_f.set_terminal_title

--- python-packages/maizy_f/__init__.py
+++ python-packages/maizy_f/__init__.py
@@ -1,4 +1,5 @@
 # _*_ coding: utf-8 _*_
+import sys
 from functools import partial
 
 
@@ -10,7 +11,19 @@ def to_unicode(val):
     return val
 
 
+def to_str(val):
+    if isinstance(val, unicode):
+        return val.encode('utf-8')
+    if not type(val) == str:
+        return str(val)  # strip pseudo str types
+    return val
+
+
 def partial_with_doc(func, _doc='', *args, **kwargs):
     partial_func = partial(func, *args, **kwargs)
     partial_func.__doc__ = _doc
     return partial_func
+
+
+def set_terminal_title(title):
+    sys.stdout.write('\x1b]2;{}\x07'.format(to_str(title)))
