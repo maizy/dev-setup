695a284b05bd46d6681c8c32799f197db7ccf2dc
Nikita Kovaliov <nikita@maizy.ru>
2017-03-18 18:24:28 +0300

    dev_projects: add bitbucket clone

--- dev_projects/functions.sh
+++ dev_projects/functions.sh
@@ -4,20 +4,29 @@ function log {
     echo -e "${PURPLE}$1${NC}"
 }
 
-function github_clone {
-    repo="$1"
-    name="$2"
+function _clone {
+    base_git_path="$1"
+    repo="$2"
+    name="$3"
     cwd=`pwd`
     if [ -z "$name" ];then
         [[ $repo =~ .*\/(.*) ]]
         name="${BASH_REMATCH[1]}"
     fi
     repo_path="$cwd/$name"
-    git_path="git@github.com:$repo"
+    git_path="${base_git_path}:${repo}.git"
     if [ ! -d "$repo_path" ]; then
         log "clone '$git_path' to '$repo_path'"
-        git clone 'git@github.com:'"$repo" "$repo_path"
+        git clone "$git_path" "$repo_path"
     else
         log "$git_path' found at '$repo_path', skipping"
     fi
+}
+
+function github_clone {
+    _clone 'git@github.com' "$1" "$2"
+}
+
+function bitbucket_clone {
+    _clone 'git@bitbucket.org' "$1" "$2"
 }
