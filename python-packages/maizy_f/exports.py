c6e73372b8bd8d9ca890444e596384bd55b328af
Nikita Kovalev <nikita@maizy.ru>
2018-01-10 14:52:57 +0300

    f aws.s3du

--- python-packages/maizy_f/exports.py
+++ python-packages/maizy_f/exports.py
@@ -1,4 +1,4 @@
 # coding: utf-8
 
-from maizy_f import netstat, git, fs, r, vbox, ssh
+from maizy_f import netstat, git, fs, r, vbox, ssh, aws
 from maizy_f.root import *
--- python-packages/setup.py
+++ python-packages/setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name='maizy_f',
     version='0.1',
-    packages=['maizy_f']
+    packages=['maizy_f'],
+    install_requires=[
+        'humanize',
+    ],
 )
